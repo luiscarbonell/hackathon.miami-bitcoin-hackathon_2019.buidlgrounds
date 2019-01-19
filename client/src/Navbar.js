ac019f2d19d37ee12c24c70ffdfaec35f14312d0
oriana <orbiarve@gmail.com>
2019-01-19 12:08:59 -0500

    install package

---
+++ client/src/Navbar.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+
+import './App.css';
+
+class Navbar extends Component {
+  
+
+
+
+
+
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar/>
+        <div>
+          
+
+
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
---
+++ client/src/course.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import {
+  Link,
+} from 'react-router-dom';
+import logo from './logo.svg';
+import Navbar from './Navbar.js'
+import './App.css';
+
+class Course extends Component {
+  
+
+
+
+
+
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar/>
+        <div>
+          
+
+
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Course;
