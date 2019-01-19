1f6137ae2b179f620af1b05c0e7ba024f847f0f3
oriana <orbiarve@gmail.com>
2019-01-19 17:54:23 -0500

    Course.js Refactoring

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Course from './Course.js'
+import Course from './course.js'
 import './index.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import App from './App';
@@ -11,7 +11,7 @@ ReactDOM.render(
 <Router>
     <div>
         <Route exact path='/'component={App}/> 
-        <Route exact path='/course' component={Course} />    
+        <Route exact path='/course' component={Course} /> 
     </div>
 </Router>, document.getElementById('root'));
 registerServiceWorker();
