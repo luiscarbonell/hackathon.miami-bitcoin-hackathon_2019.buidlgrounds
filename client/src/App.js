ae00cad68780f3933efeec6ed94042a0bec28dce
abel30567 <claudio@imarkett.com>
2019-01-19 22:25:31 -0500

    styles

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Banner from './Banner.png';
 import Course from './Course.js'
 import './App.css';
 import boo from './Design.jpeg';
+import rpi from './assets/rpibtc.jpg';
 
 import {
   Link,
@@ -12,64 +13,32 @@ import {
 class App extends Component {
 
 
-
-
   render() {
 return (
       <div className="App">
-
-        <div>
-     
-        </div>
         <div>
           <h1> Bitcoin, simple and easy </h1>
           <h4>Bitcoin for developers, directly from your browser</h4>
-
         </div>
         <div>
-           
-              <a href='/Course' role= "button" className= "btn btn-danger"> Start learning</a>
-
-
+          <a href='#Lessons' role= "button" className= "btn btn-danger"> Start learning</a>
         </div>
-
         <div className= " img">
           <img src= {Banner}/>
         </div>
 
 
-        <div>
-          
-
+        <div className="col-xs-12" id="Lessons">
+            <div className="toCourse col-xs-12">
+              <img style={{width: '60%'}} src={rpi} />
+            </div>
+            <div className="toCourse col-xs-12">
+            <img style={{width: '60%'}} src={rpi} />
+            </div>
+            <div className="toCourse col-xs-12">
+            <img style={{width: '60%'}} src={rpi} />
+            </div>
         </div>
-
-        <div>
-
-     
-            <a href='/Course' role= "button"  className= "btn btn-outline-primary"> Start learning</a>
-
-       
-
-          
-
-
-        </div>
-
-
-
-    
-
-       
-       <div className='Banner'>
-
-
-       
-       </div>
-      
-
-
-
-
       </div>
     );
   }
